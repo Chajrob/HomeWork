fd3e918ee8f9a65cda1cef2f9c4d64df31f70423
NGD <husseinn_369@yahoo.com>
2024-11-02 12:55:04 +0300

    Update module_1_peremennie.py

--- module_1_peremennie.py
+++ module_1_peremennie.py
@@ -1,5 +1,5 @@
-kurs = 'Python'
 vsego_zadach = 12
 zatracheno_chasov = 1.5
+kurs = 'Python'
 srednee_vremya_vipolneniya = zatracheno_chasov / vsego_zadach
 print ('kurs: ', kurs, ', vsego zadach: ', vsego_zadach, ', zatracheno chasov: ', zatracheno_chasov, ', srednee vremya vipolneniya ', srednee_vremya_vipolneniya, ' chasa')
