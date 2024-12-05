888ae033299308f259037cbe88bf81e6411dba01
Guseyn <husseinn_369@yahoo.com>
2024-12-06 01:46:16 +0300

    module_2_4_(second_variant)

--- module_2_4.py
+++ module_2_4.py
@@ -3,6 +3,24 @@ primes = []
 not_primes = []
 for i in numbers[1:]:
     k = 0
+    for j in numbers[1:i-1]:
+        if i % j == 0:
+            k += 1
+            break
+    if k > 0:
+        not_primes += [i]
+    else:
+        primes += [i]
+print('Primes:', primes)
+print('Not Primes:', not_primes)
+
+"""
+Первый вариант
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers[1:]:
+    k = 0
     for j in numbers[1:i]:
         if i % j == 0:
             k += 1
@@ -13,3 +31,4 @@ for i in numbers[1:]:
         primes += [i]
 print('Primes:', primes)
 print('Not Primes:', not_primes)
+"""
