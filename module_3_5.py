11175ef9b95fb1b79ddaedff76a0938a4c5ecabc
Guseyn <husseinn_369@yahoo.com>
2025-01-01 22:22:57 +0300

    module_3_5

---
+++ module_3_5.py
@@ -0,0 +1,13 @@
+def get_multiplied_digits(number):
+    while number % 10 == 0:
+        number /= 10
+    str_number = str(int(number))
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+print(get_multiplied_digits(40203))
+print(get_multiplied_digits(402030))
+
